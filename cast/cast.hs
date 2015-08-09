1c1bd1a6191e4caeba62e1d1544085c56729fc0c
Docteur-Lalla <kevin.le_bon@yahoo.fr>
2015-08-09 09:54:50 +0200

    Add a boolean casting function

--- cast/cast.hs
+++ cast/cast.hs
@@ -34,6 +34,18 @@ float [val@(Number (Float _))] = return val
 float [x] = throwError $ TypeMismatch "string or number" x
 float values = throwError $ NumArgs 1 values
 
+boolean :: [YmirValue] -> ThrowsError YmirValue
+boolean [String "false"] = return $ Bool False
+boolean [String ""] = return $ Bool False
+boolean [Atom "false"] = return $ Bool False
+boolean [Atom ""] = return $ Bool False
+boolean [Bool b] = return $ Bool b
+boolean [Number (Integer 0)] = return $ Bool False
+boolean [Number (Float 0.0)] = return $ Bool False
+boolean [List []] = return $ Bool False
+boolean [_] = return $ Bool True
+boolean values = throwError $ NumArgs 1 values
+
 -- C bindings
 c_bind_function :: HaskellYmirFunction -> Ptr ValuePtr -> Int -> ReturnValuePtr
 c_bind_function f ary n = unsafePerformIO $
@@ -46,8 +58,10 @@ c_string = c_bind_function string
 c_symbol = c_bind_function symbol
 c_integer = c_bind_function integer
 c_float = c_bind_function float
+c_boolean = c_bind_function boolean
 
 foreign export ccall c_string :: Ptr ValuePtr -> Int -> ReturnValuePtr
 foreign export ccall c_symbol :: Ptr ValuePtr -> Int -> ReturnValuePtr
 foreign export ccall c_integer :: Ptr ValuePtr -> Int -> ReturnValuePtr
 foreign export ccall c_float :: Ptr ValuePtr -> Int -> ReturnValuePtr
+foreign export ccall c_boolean :: Ptr ValuePtr -> Int -> ReturnValuePtr
