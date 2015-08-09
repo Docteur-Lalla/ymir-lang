b8064ea585b2036ae06f947d744c6150b4dfb3cf
Docteur-Lalla <kevin.le_bon@yahoo.fr>
2015-08-09 10:39:29 +0200

    Add pow, ln, log10 and log2 functions to math library

--- math/math.c
+++ math/math.c
@@ -30,6 +30,9 @@ YMIR_C_FUNCTION(tanh, c_tanh);
 YMIR_C_FUNCTION(acosh, c_acosh);
 YMIR_C_FUNCTION(asinh, c_asinh);
 YMIR_C_FUNCTION(atanh, c_atanh);
+YMIR_C_FUNCTION(log, c_log);
+YMIR_C_FUNCTION(log10, c_log10);
+YMIR_C_FUNCTION(log2, c_log2);
 
 YMIR_C_FUNCTION(exp, c_exp);
 
@@ -68,5 +71,26 @@ YmirValue c_floor(YmirArray args, int size)
       double arg = ymir_getFloat(args[0]);
       YMIR_RETURN(ymir_newFloat(floor(arg)));
     }
+  }
+}
+
+YmirValue c_pow(YmirArray args, int size)
+{
+  if(size != 2)
+    YMIR_THROW_NUMBER_ARGS(2, args, size);
+  else if(!ymir_isNumber(args[0]))
+    YMIR_THROW_TYPE_MISMATCH("number", args[0]);
+  else if(!ymir_isNumber(args[1]))
+    YMIR_THROW_TYPE_MISMATCH("number", args[1]);
+  else
+  {
+    double x = ymir_isFloat(args[0]) ?
+      ymir_getFloat(args[0]) :
+      (double)ymir_getInteger(args[0]);
+    double n = ymir_isFloat(args[1]) ?
+      ymir_getFloat(args[1]) :
+      (double)ymir_getInteger(args[1]);
+    
+    YMIR_RETURN(ymir_newFloat(pow(x, n)));
   }
 }
