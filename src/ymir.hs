9c135dd0c97363b72cbc67ca620818e0b5e1ece1
Camille Le Bon <camille.lebon35000@gmail.com>
2020-04-15 15:42:16 +0200

    Clean ymir.hs a bit

--- src/ymir.hs
+++ src/ymir.hs
@@ -14,14 +14,16 @@ import System.Directory (getCurrentDirectory)
 flushStr :: String -> IO ()
 flushStr str = putStr str >> hFlush stdout
 
+-- |Print a prompt asking the user for input and then read a line of code.
 readPrompt :: String -> IO String
 readPrompt prompt = flushStr prompt >> getLine
 
+-- |Parse Ymir code and evaluate it.
 evalString :: Env -> String -> IO String
-evalString env exp = runIOThrows $ liftM show $ lifting >>= eval env
-  
+evalString env exp = runIOThrows $ fmap show $ lifting >>= eval env
   where lifting = liftThrows $ readExpr exp
 
+-- |Evaluate an expression and then print its result.
 evalAndPrint :: Env -> String -> IO ()
 evalAndPrint env expr = evalString env expr >>= putStrLn
 
@@ -33,26 +35,26 @@ until_ pred prompt action =
       then return ()
       else action result >> until_ pred prompt action
 
+-- |Load each file given as paremeter and run them one after the other.
 runOne :: [String] -> IO ()
 runOne args =
   do
     dir <- getCurrentDirectory
     env <- primitiveBindings >>= flip bindVars (binds dir)
-    (runIOThrows $ liftM show $ res env) -- >>= hPutStrLn stderr
+    runIOThrows $ show <$> res env -- >>= hPutStrLn stderr
     return ()
-
   where
     binds dir = [("args", List list), ("@@file", String ""), ("@@dir", String dir)]
     list = map String $ drop 1 args
-    res env = eval env (List [Atom "require-relative", String (args !! 0)])
+    res env = eval env (List [Atom "require-relative", String (head args)])
 
+-- |Run the interactive interpreter.
 runRepl :: IO ()
 runRepl =
   do
     dir <- getCurrentDirectory
     bindings <- primitiveBindings >>= flip bindVars [atatfile, ("@@dir", String dir)]
     (until_ (== "exit") (readPrompt "ymir: ") . evalAndPrint) bindings
-
   where
     atatfile = ("@@file", String "")
 
@@ -62,4 +64,4 @@ main =
     args <- getArgs
     if null args
       then runRepl
-      else runOne $ args
+      else runOne args
